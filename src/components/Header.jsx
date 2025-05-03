3269ed7f6944df2ec674a437300890b8ae16e1c4
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-05-03 12:04:55 +0200

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,16 @@ import SearchBar from "./Searchbar.jsx";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div>
+      <div className={styles.content}>
         <img className={styles.logo} src={logo} alt="Story Time Logo" />
-        <div className={styles.content}>
-          <div className={styles.navigation}>
-            <a href="/">Home</a>
-            <a href="/projects">Projects</a>
-            <a href="/about">making-of</a>
-          </div>
-          <SearchBar/>
+        <div className={styles.navigation}>
+          <a href="/">Home</a>
+          <a href="/projects">Projects</a>
+          <a href="/about">making-of</a>
         </div>
+        {/* <SearchBar/> */}
       </div>
+      <div className={styles.couldsBackground} />
     </header>
   );
 };
