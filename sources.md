e601b126749bd891b8824ba03f63e6e39931dec8
Staf dierickx <114059414+StafDierickx@users.noreply.github.com>
2025-05-07 16:54:24 +0200

    add react router

---
+++ sources.md
@@ -0,0 +1,6 @@
+React context
+- https://react.dev/reference/react/createContext
+- https://www.w3schools.com/react/react_usecontext.asp
+
+react routes
+- https://reactrouter.com/start/declarative/routing
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 
-import Story from './components/Story.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+
+import { BrowserRouter, Routes, Route} from 'react-router'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+
+import Home from './pages/Home.jsx'
+import Layout from './pages/Layout.jsx'
+
+import StoryPage from './pages/StoryPage.jsx'
 
 function App() {
   return (
     <>
-      <Header />
-      <Story />
-      <Footer />
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+
+            <Route index element={<Home />} />
+            <Route path='/' element={<Layout />}>
+              <Route path="story" element={<StoryPage />} />
+            </Route> 
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 }
