bd3fa646e0df3c312296d62f6aeeac8266a7bded
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-05-13 12:28:55 +0200

    Create tile & tilegrid module

---
+++ src/components/Tile.jsx
@@ -0,0 +1,43 @@
+import { useState, useRef } from "react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+import Styles from "../styles/components/Tile.module.css"
+import { style } from "framer-motion/m";
+
+import img from "../assets/dummy.jpeg"
+
+function Tile({title, creator, image}) {
+    const animations = {
+        fadeIn: {
+            height: "auto",
+            opacity: 1,
+            transition: {
+                duration: 0.3,
+                ease: "easeInOut"
+            }
+        },
+        fadeout: {
+            height: 0,
+            // margin:0,
+            opacity: 0,
+            transition: {
+                duration: 0.3,
+                ease: "easeInOut"
+            }
+        }
+    };
+
+    return (
+        <div className={Styles.tile}>
+            <img src={img} alt="" />
+            <div>
+                <div className={Styles.content}>
+                    <p className={Styles.title}>{title}</p>
+                    <p>{creator}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Tile
---
+++ src/components/TileGrid.jsx
@@ -0,0 +1,43 @@
+import { useState, useRef } from "react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+import Styles from "../styles/components/Tile.module.css"
+import { style } from "framer-motion/m";
+
+import Tile from "./Tile"
+
+import img from "../assets/dummy.jpeg"
+
+function TileGrid({data, max = 0}) {
+    const animations = {
+        fadeIn: {
+            height: "auto",
+            opacity: 1,
+            transition: {
+                duration: 0.3,
+                ease: "easeInOut"
+            }
+        },
+        fadeout: {
+            height: 0,
+            // margin:0,
+            opacity: 0,
+            transition: {
+                duration: 0.3,
+                ease: "easeInOut"
+            }
+        }
+    };
+
+    const displayData = max > 0 ? data.slice(0, max) : data;
+
+    return (
+        <div className={Styles.tileGrid}>
+            {displayData.map((entry, index) => {
+                return <Tile key={index} {...entry}/>
+            })}
+        </div>
+    );
+}
+
+export default TileGrid
