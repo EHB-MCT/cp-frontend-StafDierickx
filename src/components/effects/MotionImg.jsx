c4f3288e834b42f42be4b12d669071c0039c7166
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-05-20 15:12:49 +0200

    Add MotionImg component
    This component exists to separete the concern of the isInView logic.
    This way I don't have to clutter and mentally manage a bunch of them for each elemen in Story.jsx

---
+++ src/components/effects/MotionImg.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
+
+// this component exists to separete the concern of the isInView logic.
+// This way I don't have to clutter and mentally manage a bunch of them for each elemen in Story.jsx
+
+function MotionImg({ src, className, initial, animate, exit, transition }) {
+  const ref = React.useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  if (!initial) initial = { x: 15, opacity: 0 };
+  if (!animate)
+    animate = { x: 0, opacity: 1 };
+  if (!exit) exit = { opacity: 0 };
+  if (!transition)
+    transition = {duration: 0.5, delay: 0.5}
+  return (
+    <motion.img
+      src={src}
+      ref={ref}
+      className={className}
+      initial={initial}
+      animate= { isInView ? animate : {opacity:0}}
+      exit={exit}
+      transition={transition}
+    />
+  );
+}
+
+export default MotionImg;
