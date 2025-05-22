441358095df59ce0f56a01659441ff3068a7a3a4
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-05-22 11:10:09 +0200

    Fetch fairy tale data on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import Header from "@/components/ui/Header.jsx";
 
@@ -9,22 +9,41 @@ import { BannerExpandContext } from "@/context/BannerContext.jsx";
 
 import Styles from "@/styles/pages/Home.module.css"
 
-import dummydata from "@/assets/dummy-data.json"
+// import dummydata from "@/assets/dummy-data.json"
 
 function Home({expand = true}) {
   const { bannerExpand, setBannerExpand } = useContext(BannerExpandContext)
   setBannerExpand(true)
 
-  let storyCards = dummydata
+  let [storyCards, setStoryCards] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  
+  useEffect(() => {
+    setIsLoading(true)
+    
+    // fetch json array from api
+    console.log("Fetching sprookjes data...")
+    fetch("https://raw.githubusercontent.com/EHB-MCT/cp-frontend-MaximWesterbeek/refs/heads/main/course-project/public/api/fairytaleList.json")
+      .then(response => response.json())
+      .then(data => {
+        console.log("Data fetched:", data)
+        
+        setStoryCards([...storyCards].sort(() => Math.random() - 0.5));
+        setIsLoading(false)
+      })
+      .catch(error => console.error(error))
+  }, [])
 
   return (
     <>
         {/* <Header expand={true} /> */}
         <br />
         <SectionHeader text="Populair" />
-        <div className="center-body">
-          <TileGrid data={storyCards} max='8' />
-        </div>
+        { !isLoading && 
+          <div className="center-body">
+            <TileGrid data={storyCards} max='8' />
+          </div>  
+        }
     </>
   )
 }
