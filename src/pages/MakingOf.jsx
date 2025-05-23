29a0d003e0b2c4f3e1261f83c602cd26b3588b51
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-05-23 16:08:45 +0200

    Add load data logic in making off page

--- src/pages/MakingOf.jsx
+++ src/pages/MakingOf.jsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from "react";
 import SectionHeader from "@/components/SectionHeader"
 import Styles from "@/styles/pages/MakingOf.module.css";
 import dummydata from "@/assets/dummy-data.json"
-import img from "@/assets/dummy.jpeg"
+import notFound from "@/assets/img-not-found.png"
+
+const TitleDivider = ({text}) => {
+    return (
+        <div className={Styles.titleDivider}>
+            <p>{text}</p>
+            <hr />
+        </div>
+    )
+}
 
 function MakingOf() {
     const [data, setData] = useState(null);
@@ -10,8 +19,8 @@ function MakingOf() {
     useEffect(() => {
         console.log("Loading dummy data...");
 
-        let data = dummydata[0]
-        data.image = img
+        // let data = dummydata[0]
+        // data.image = img
 
         setData(dummydata[0]);
     }, []); // Empty dependency array means this effect runs once on mount
@@ -21,33 +30,44 @@ function MakingOf() {
             <SectionHeader text="Making of" />
             <div className={Styles.makingOf}>
                 <div className={Styles.bannerImg}>
-                    <img src={data && data.image} alt="" />
+                    <img
+                        src={data && data.imgBanner ? data.imgBanner : notFound}
+                        alt=""
+                    />
                     <div>
                         <div>
-                            <p className={Styles.storyTitle}> {data && data.title}</p>
-                            <p className={Styles.creator}>{data && data.creator}</p>
+                            <p className={Styles.storyTitle}> {data && data.fairytale}</p>
+                            <p className={Styles.creator}>{data && data.nameStudent}</p>
                         </div>
                     </div>
                 </div>
                 <div>
                     <div>
-                        <p className={Styles.title}>Video - Explainer Movie</p>
+                        <TitleDivider text={"video - explainer movie"} />
                     </div>
-                    <div>
+                    <div className={Styles.videoExplainer}>
                         <div>
-                            {data && data.video}
+                            {data && data.videoExplainer ? data.videoExplainer : "No video explainer available"}
                         </div>
                         <div>
-
+                            {data && data.description}
                         </div>
                     </div>
                 </div>
                 <div>
                     <div>
-                    <p className={Styles.title}>Extra beeldmateriaal</p>
+                    <TitleDivider text={"extra beeldmateriaal"} />
                     </div>
-                    <div>
-                        {data && JSON.stringify(data.extraMaterial)}
+                    <div className={Styles.ExtraImage}>
+                        {data &&
+                        data.imgsExtra ?
+                        data.imgsExtra.map((item, index) => (
+                            <img 
+                                key={index}
+                                src={item}
+                            />
+                        )) :
+                        (<img src={notFound} />)}
                     </div>
                 </div>
             </div>
