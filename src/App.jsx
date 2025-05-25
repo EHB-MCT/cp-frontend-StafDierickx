4282d6dba0a668723461382ce5fbb349635e51fd
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-05-25 17:44:53 +0200

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 
-
 import { BrowserRouter, Routes, Route} from 'react-router'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import BannerProvider from './context/BannerContext.jsx'
@@ -9,7 +8,7 @@ import BannerProvider from './context/BannerContext.jsx'
 import Layout from './pages/Layout.jsx'
 import Home from './pages/Home.jsx'
 import Sprookjes from './pages/Sprookjes.jsx'
-import StoryPage from './pages/StoryPage.jsx'
+import ParalaxPage from './pages/Paralax.jsx'
 import MakingOf from './pages/MakingOf.jsx'
 
 function App() {
@@ -22,9 +21,10 @@ function App() {
               <Route path='/' element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="sprookjes" element={<Sprookjes />} />
-                <Route path="story" element={<StoryPage />} />
+                <Route path="story" element={<ParalaxPage />} />
                 <Route path="making-of" element={<MakingOf />} />
               </Route> 
+              <Route path="paralax" element={<ParalaxPage />} />
             </Routes>
           </BrowserRouter>
         </BannerProvider>
---
+++ src/pages/Paralax.jsx
@@ -0,0 +1,9 @@
+import Story from "@/components/Story.jsx"
+
+function ParalaxPage() {
+    return (
+        <Story />
+    )
+}
+
+export default ParalaxPage
